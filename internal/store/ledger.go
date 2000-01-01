9cf3dc78b6d92d01b9275639eecfe4ca94a9d6a7
Benzhi Claimant <claimant@localhost>
2000-01-01 00:00:00 +0000

    Client result for a4cfdd67-9a14-48d0-ad87-ba4802711ac0-bug-3

--- internal/store/ledger.go
+++ internal/store/ledger.go
@@ -58,25 +58,86 @@ func clone(s Snapshot) Snapshot {
 	n := EmptySnapshot()
 	n.SchemaVersion = s.SchemaVersion
 	for k, v := range s.Batches {
-		n.Batches[k] = v
+		n.Batches[k] = cloneBatch(v)
 	}
 	for k, v := range s.Sources {
-		n.Sources[k] = v
+		n.Sources[k] = cloneSource(v)
 	}
 	for k, v := range s.Reviews {
-		n.Reviews[k] = v
+		n.Reviews[k] = cloneReview(v)
 	}
 	for k, v := range s.Snapshots {
-		n.Snapshots[k] = v
+		n.Snapshots[k] = cloneSnapshot(v)
 	}
 	for k, v := range s.Certificates {
 		n.Certificates[k] = v
 	}
-	n.Events = append([]model.AuditEvent(nil), s.Events...)
+	n.Events = make([]model.AuditEvent, len(s.Events))
+	for i, e := range s.Events {
+		n.Events[i] = cloneEvent(e)
+	}
 	for k, v := range s.Idempotency {
 		n.Idempotency[k] = append([]byte(nil), v...)
 	}
 	return n
+}
+func cloneBatch(b model.DatasetBatch) model.DatasetBatch {
+	if b.ReleaseScope != nil {
+		b.ReleaseScope = append([]string(nil), b.ReleaseScope...)
+	}
+	return b
+}
+func cloneSource(s model.SourceRecord) model.SourceRecord {
+	if s.EvidenceRefs != nil {
+		s.EvidenceRefs = append([]string(nil), s.EvidenceRefs...)
+	}
+	return s
+}
+func cloneReview(r model.ReviewTask) model.ReviewTask {
+	if r.Issues != nil {
+		r.Issues = append([]string(nil), r.Issues...)
+	}
+	if r.EvidenceRefs != nil {
+		r.EvidenceRefs = append([]string(nil), r.EvidenceRefs...)
+	}
+	return r
+}
+func cloneSnapshot(s model.ReleaseSnapshot) model.ReleaseSnapshot {
+	if s.ApprovedScope != nil {
+		s.ApprovedScope = append([]string(nil), s.ApprovedScope...)
+	}
+	return s
+}
+func cloneEvent(e model.AuditEvent) model.AuditEvent {
+	if e.Payload != nil {
+		e.Payload = clonePayload(e.Payload)
+	}
+	return e
+}
+func clonePayload(p map[string]any) map[string]any {
+	out := make(map[string]any, len(p))
+	for k, v := range p {
+		out[k] = cloneValue(v)
+	}
+	return out
+}
+func cloneValue(v any) any {
+	switch x := v.(type) {
+	case []string:
+		return append([]string(nil), x...)
+	case []any:
+		out := make([]any, len(x))
+		for i, e := range x {
+			out[i] = cloneValue(e)
+		}
+		return out
+	case map[string]any:
+		return clonePayload(x)
+	case []byte:
+		return append([]byte(nil), x...)
+	default:
+		return v
+	}
 }
 func validate(s Snapshot) error {
 	if s.SchemaVersion != SchemaVersion {
